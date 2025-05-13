11b5bf8c593f7fe226700d63f00df9386800cc61
balu2422 <157208069+balu2422@users.noreply.github.com>
2025-05-13 12:02:35 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "name_prefix" {
+  default = "docker-demo"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  default = "10.0.1.0/24"
+}
+
+variable "availability_zone" {
+  default = "us-east-1a"
+}
+
+variable "ami_id" {}
+variable "instance_type" {}
+variable "key_name" {}
