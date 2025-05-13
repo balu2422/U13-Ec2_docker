254e361599a50bd59cf98d13b1417cda746f589b
balu2422 <157208069+balu2422@users.noreply.github.com>
2025-05-13 12:02:13 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+module "vpc" {
+  source              = "./modules/vpc"
+  name_prefix         = var.name_prefix
+  vpc_cidr            = var.vpc_cidr
+  public_subnet_cidr  = var.public_subnet_cidr
+  availability_zone   = var.availability_zone
+}
+
+module "ec2" {
+  source        = "./modules/ec2"
+  ami_id        = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+  subnet_id     = module.vpc.public_subnet_id
+  name_prefix   = var.name_prefix
+}
